42d204c1ee00b3bfde9771a672852d55ec350458
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2022-08-01 12:59:00 -0400

    Update Fruitifinder.py

--- Fruitifinder.py
+++ Fruitifinder.py
@@ -1,19 +1,22 @@
 import requests
 import blackboxprotobuf
+import sys
 
 def apple_bssid(bssid):
+    print(bssid)
     x = {}
     x['bssid'] = bssid
     data_bssid = f'\x12\x13\n\x11{bssid}\x18\x00\x20\01'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded',
-               'Accept': '*/*',
-               "Accept-Charset": "utf-8",
-               "Accept-Encoding": "gzip, deflate",
-               "Accept-Language": "en-us",
-               'User-Agent': 'locationd/1776 CFNetwork/711.1.12 Darwin/14.0.0'
-               }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = '\x00\x01\x00\x05en_US\x00\x13com.apple.locationd\x00\x0a8.1.12B411\x00\x00\x00\x01\x00\x00\x00' + chr(len(data_bssid)) + data_bssid
     response = requests.post('https://gs-loc.apple.com/clls/wloc', headers=headers, data=data)
     x['lat'] = blackboxprotobuf.decode_message(response.content)[0]['2'][0]['2']['1']/10**8
     x['lon'] = blackboxprotobuf.decode_message(response.content)[0]['2'][0]['2']['2']/10**8
     return x
+
+def main():
+    found = apple_bssid(sys.argv[0])
+    print("found " + found['bssid'] + " at https://google.com/maps/dir/" + str(found['lat']) + ',' + str(found['lon']))
+
+if __name__=="__main__":
+    main()
