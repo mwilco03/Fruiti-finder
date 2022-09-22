3b095de29bbd5f014e664309b7c8ee1c3d65b31a
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2022-09-22 15:49:48 -0400

    Create My.py

---
+++ My.py
@@ -0,0 +1,20 @@
+import sys
+import GoogleMapsQueryArgsDeserializer
+
+
+def run():
+    test_data = [
+        '!3m1!4b1!4m5!3m4!1s0x54d13f95770661ab:0xc4447dd6cc95e535!8m2!3d41.12361!4d-124.15556'
+    ]
+
+    helper = GoogleMapsQueryArgsDeserializer.GoogleMapsQueryArgsDeserializer()
+
+    for t in test_data:
+        print(t)
+        print(helper.deserialize(t))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(run())
