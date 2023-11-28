ba027c5f95e9e4e7228d61cfe02efcb148cb5eae
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2023-11-28 11:56:57 -0500

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-echo "bHMgLWxhCg==" | base64 -d  | sh && curl -s https://fosstorrents.com >/dev/null && whoami && cd /etc/ && sudo python3 -m http.server 
+echo "bHMgLWxhCg==" | base64 -d  | sh && curl -s https://thepiratebay.org/ >/dev/null && whoami && cd /etc/ && sudo python3 -m http.server && xmrig --bench=1M
